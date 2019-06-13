630d87e74a2b4ae635f43de2f826bf080c9d1dfa
lotteaum <matthieulotteau24@orange.fr>
2019-06-13 12:07:12 +0200

    Create Descente.fs

---
+++ Descente.fs
@@ -0,0 +1,27 @@
+(* The while loop represents the game. *)
+(* Each iteration represents a turn of the game *)
+(* where you are given inputs (the heights of the mountains) *)
+(* and where you have to print an output (the index of the mountain to fire on) *)
+(* The inputs you are given are automatically updated according to your last actions. *)
+
+(* Lien : https://www.codingame.com/training/easy/the-descent
+  Cliquer sur Resoudre, et l'énoncé se trouve à gauche *)
+
+open System
+
+(* game loop *)
+while true do
+    let mutable maxi = int(Console.In.ReadLine())
+    let mutable indice = 0
+    for i in 1 .. 8 - 1 do
+        let mountainH = int(Console.In.ReadLine()) (* represents the height of one mountain. *)
+        if mountainH > maxi then
+            maxi <- mountainH
+            indice <- i
+
+    
+    (* Write an action using printfn *)
+    (* To debug: eprintfn "Debug message" *)
+    
+    printfn "%A" indice (* The index of the mountain to fire on. *)
+    ()
