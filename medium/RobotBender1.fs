3771392ab9a7774941e5828c4b55fea61602e9b4
lotteaum <matthieulotteau24@orange.fr>
2019-06-16 15:16:26 +0200

    Update and rename RobotBender.fs to medium/RobotBender1.fs

--- medium/RobotBender1.fs
+++ medium/RobotBender1.fs
@@ -1,7 +1,8 @@
 (* Auto-generated code below aims at helping you parse *)
 (* the standard input according to the problem statement. *)
 
-(* Lien : https://www.codingame.com/ide/puzzle/bender-episode-1
+(* Lien : https://www.codingame.com/training/medium/bender-episode-1
+
     Cliquer sur Resoudre, et l'énoncé se trouve à gauche
     Choisir F# comme langage de programmation *)
 
